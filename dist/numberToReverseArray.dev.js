202a8aaeafe89bc32533ec4fd525fbea0cf97c84
Matthew-Cocking <89878310+Matthew-Cocking@users.noreply.github.com>
2022-05-16 00:39:34 +0100

    Delete dist directory

--- dist/numberToReverseArray.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
-function digitize(n) {
-  var string = n.toString();
-  var array = Array.from(string).map(Number);
-  array.reverse();
-  return array;
-}
